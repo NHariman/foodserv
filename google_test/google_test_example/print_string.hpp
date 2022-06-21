bee5ba43954ae1fa2e2a0af81cd085d4c0aee970
Michelle Jiam <mjiam@student.codam.nl>
2022-06-21 16:49:32 +0200

    Moved gtest_example to google_test dir; Added build example - chmod & ./gotest.sh to use

---
+++ google_test/google_test_example/print_string.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   print_string.hpp                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/04/16 18:04:00 by nhariman      #+#    #+#                 */
+/*   Updated: 2022/04/16 21:10:58 by nhariman      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+
+
+std::string     print_string(std::string str, std::string add);
+int             addition(int a, int b);
+
