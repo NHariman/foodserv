e5b82259ef6586e380dd11ae9205826d713eb5ce
Michelle Jiam <mjiam@student.codam.nl>
2022-07-06 15:17:04 +0200

    Turned loop stop condition & incrementation into hooks for request parsing

--- src/request/state_parser.hpp
+++ src/request/state_parser.hpp
@@ -5,16 +5,17 @@ template <typename State>
 class StateParser {
 	public:		
 		void	Parse(string const& input_string) {
-			size_t i;
+			size_t i = 0;
 
 			PreParseCheck();
 			cur_state = SetStartState();
-			for (i = 0; i <= input_string.size(); i++) {
+			while (NotDone(i)) {
 				cur_state = GetNextState(input_string[i]);
 				InvalidStateCheck();
 				if (DoneStateCheck())
 					break;
 				UpdateBuffer(input_string[i]);
+				IncrementCounter(i);
 			}
 			AfterParseCheck(i);
 		}
@@ -32,11 +33,20 @@ class StateParser {
 		virtual void	UpdateBuffer(char c) {
 			buffer += c;
 		}
-		// Hooks may be overriden by subclasses, but it's not mandatory since
+		// Hooks are optional extensions points where subclasses can add actions.
+		// They can be overriden by subclasses, but it's not mandatory since
 		// the hooks already have default (but empty) implementation.
-		// This hook is for checks before entering parsing loop, e.g. if input string exceeds max allowed size.
+		// This hook is for if subclasses want to use a different stopping point.
+		bool			NotDone(size_t pos) const {
+			return (pos <= input.size());
+		}
+		// Hook for if subclasses want custom incrementation steps.
+		void			IncrementCounter(size_t& pos) {
+			pos += 1;
+		}
+		// Hook for checks before entering parsing loop, e.g. if input string exceeds max allowed size.
 		virtual void	PreParseCheck() const {};
-		// This hook is for checks after parsing, e.g. if there are characters after end state has been reached.
+		// Hook for checks after parsing, e.g. if there are characters after end state has been reached.
 		virtual void	AfterParseCheck(size_t pos) const { (void)pos; };
 };
 
