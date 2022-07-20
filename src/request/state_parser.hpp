a46b7b2fdff63941c06561e30aa2ed2544a53649
Michelle Jiam <mjiam@student.codam.nl>
2022-07-20 18:04:09 +0200

    Added comments to StateParser

--- src/request/state_parser.hpp
+++ src/request/state_parser.hpp
@@ -5,11 +5,16 @@
 
 using namespace std;
 
+// An abstract class template that defines the state parsing algorithm
+// as a template method, complete with hooks and abstract methods to be
+// implemented by the subclasses.
+// `State` template parameter refers to type of states, e.g. an enum.
+// The state parsing algorithm itself is made for parsing strings byte-by-byte.
 template <typename State>
 class StateParser {
 	public:
-		// State parsing algorithm defined as a template method.
-		// Takes string to parse (byte-by-byte) and returns bytes read.
+		// Template method
+		// Takes string to parse and returns bytes read.
 		size_t	ParseString(string const& input_string) {
 			size_t i = 0;
 
@@ -18,8 +23,8 @@ class StateParser {
 			cur_state = SetStartState();
 			while (NotDone(i)) {
 				cur_state = GetNextState(i);
-				InvalidStateCheck();
-				if (DoneStateCheck())
+				CheckInvalidState();
+				if (CheckDoneState())
 					break;
 				UpdateBuffer(i);
 				IncrementCounter(i);
@@ -30,30 +35,36 @@ class StateParser {
 
 	protected:
 		State	cur_state;
-		string	buffer;
-		string	input;
+		string	buffer; // for keeping track of parsed input
+		string	input; // saving original input
 
+		// Abstract methods that must be implemented by subclass.
 		virtual State	SetStartState() const = 0;
 		virtual State	GetNextState(size_t pos) = 0;
-		virtual void	InvalidStateCheck() const = 0;
-		virtual bool	DoneStateCheck() = 0;
+		virtual void	CheckInvalidState() const = 0;
+		virtual bool	CheckDoneState() = 0;
+
+		// Concrete methods that can be overridden if custom looping behaviour
+		// is needed.
 		virtual void	UpdateBuffer(size_t pos) {
 			buffer += input[pos];
 		}
-		// Hooks are optional extensions points where subclasses can add actions.
-		// They can be overriden by subclasses, but it's not mandatory since
-		// the hooks already have default (but empty) implementation.
-		// This hook is for if subclasses want to use a different stopping point.
 		virtual bool			NotDone(size_t pos) const {
 			return (pos <= input.size());
 		}
-		// Hook for if subclasses want custom incrementation steps.
 		virtual void			IncrementCounter(size_t& pos) {
 			pos += 1;
 		}
-		// Hook for checks before entering parsing loop, e.g. if input string exceeds max allowed size.
+
+		// Hooks. Optional extensions points where subclasses can add actions.
+		// They can be overriden by subclasses, but it's not mandatory since
+		// the hooks already have default (but empty) implementation.
+		// This hook is for if subclasses want to use a different stopping point.
+		// This is for checks before entering parsing loop, e.g. if input string
+		// exceeds max allowed size.
 		virtual void	PreParseCheck() {};
-		// Hook for checks after parsing, e.g. if there are characters after end state has been reached.
+		// This is for checks after parsing, e.g. if there are characters after
+		// end state has been reached.
 		virtual void	AfterParseCheck(size_t pos) { (void)pos; };
 };
 
