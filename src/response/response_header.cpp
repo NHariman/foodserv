57d332a8f95d2fe48de91fa534251b98353549af
Niks Hariman <nhariman@hotmail.com>
2022-08-02 20:56:01 +0200

    started response header creating

---
+++ src/response/response_header.cpp
@@ -0,0 +1,22 @@
+/*
+ __      _____| |__  ___  ___ _ ____   __ 
+ \ \ /\ / / _ \ '_ \/ __|/ _ \ '__\ \ / /   
+  \ V  V /  __/ |_) \__ \  __/ |   \ V /  
+   \_/\_/ \___|_.__/|___/\___|_|    \_/   
+   
+Response header aggregates information on the
+context of the response given after the server responds
+to a request.
+*/
+
+// https://developer.mozilla.org/en-US/docs/Glossary/Response_header
+
+/*
+
+    what I need:
+    - type of request, GET, POST, DELETE
+    
+
+
+
+*/
---
+++ src/response/response_header.hpp
@@ -0,0 +1,25 @@
+/*
+ __      _____| |__  ___  ___ _ ____   __ 
+ \ \ /\ / / _ \ '_ \/ __|/ _ \ '__\ \ / /   
+  \ V  V /  __/ |_) \__ \  __/ |   \ V /  
+   \_/\_/ \___|_.__/|___/\___|_|    \_/   
+   
+   The class NginxConfig parses the configuration
+   file given as argument.
+*/
+
+/*
+
+	what I need:
+	- type of request, GET, POST, DELETE
+	
+*/
+
+#ifndef RESPONSE_HEADER_HPP
+# define RESPONSE_HEADER_HPP
+
+class   ServerResponse {
+
+};
+
+#endif
