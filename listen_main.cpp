0cf0c5b6775f83235e9fd47d44b184554a383bcb
Sanne Albreghs <55091762+salbregh@users.noreply.github.com>
2022-08-30 22:38:44 +0200

    more listen

--- listen_main.cpp
+++ listen_main.cpp
@@ -2,7 +2,7 @@
 
 int     main() {
 	try {
-		Listen  test1("127.0.0.1:8000");
+		Listen  test1("0:");;
 		std::cout << "test1 IP: " << test1.getIpNumber() << " port: " << test1.getPortNumber() << std::endl;
 		
 		Listen  test2("8000");
@@ -14,8 +14,7 @@ int     main() {
 		Listen  test4("localhost:8000");
 		std::cout << "test4 IP: " << test4.getIpNumber() << " port: " << test4.getPortNumber() << std::endl;
 
-		Listen  test5("localhost");
-		std::cout << "test5 IP: " << test5.getIpNumber() << " port: " << test5.getPortNumber() << std::endl;
+
 
 		Listen  test6("[::]:8000");
 		std::cout << "test6 IP: " << test6.getIpNumber() << " port: " << test6.getPortNumber() << std::endl;
@@ -29,11 +28,14 @@ int     main() {
 		Listen	test9("");
 		std::cout << "test9 IP: " << test9.getIpNumber() << " port: " << test9.getPortNumber() << std::endl;
 
-		Listen	test10("crap");
-		std::cout << "test10 IP: " << test10.getIpNumber() << " port: " << test10.getPortNumber() << std::endl;
+		Listen	test10("12:");
+		std::cout << "test10 IP: " << test9.getIpNumber() << " port: " << test9.getPortNumber() << std::endl;
 
-		Listen	test11("cra:p");
-		std::cout << "test11 IP: " << test11.getIpNumber() << " port: " << test11.getPortNumber() << std::endl;
+		// Listen	test10("crap");
+		// std::cout << "test10 IP: " << test10.getIpNumber() << " port: " << test10.getPortNumber() << std::endl;
+
+		// Listen	test11("cra:p");
+		// std::cout << "test11 IP: " << test11.getIpNumber() << " port: " << test11.getPortNumber() << std::endl;
 
 	}
 	catch (const std::exception& e) {
--- src/config/listen.cpp
+++ src/config/listen.cpp
@@ -6,7 +6,7 @@
 /*   By: salbregh <salbregh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/07 13:10:16 by salbregh      #+#    #+#                 */
-/*   Updated: 2022/08/29 22:33:07 by salbregh      ########   odam.nl         */
+/*   Updated: 2022/08/30 22:37:23 by salbregh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@
 
 
 Listen::Listen(std::string listen) : _listen(listen), _port_number(DEFAULT_PORT), _ip_address(DEFAULT_IP) {
-	std::cout << std::endl << "String in listen: " << _listen << std::endl;
 	if (_listen.empty()) {
+		_port_number = "80";
+		_ip_address = "0";
 		return ;
 	}
 
@@ -35,7 +36,16 @@ void		Listen::SplitPortIP() {
 	std::size_t	pos;
 	pos = _listen.find_last_of(":");
 	// only IP or port is given , figure out which one
-	if (pos == std::string::npos || (_listen.front() == '[' && _listen.back() == ']')) {
+	if (pos == _listen.size() - 1) {
+		if (pos == 0) {
+			throw InvalidIpException();
+		}
+		throw InvalidPortException();
+	}
+	else if (pos == 0) {
+		throw InvalidIpException();
+	}
+	else if (pos == std::string::npos || (_listen.front() == '[' && _listen.back() == ']')) {
 		if (_listen.find('.') != std::string::npos || _listen.find(':') != std::string::npos
 			|| _listen.find("localhost") != std::string::npos || _listen.find("*") != std::string::npos) {
 			_port_number = "80";
@@ -53,11 +63,10 @@ void		Listen::SplitPortIP() {
 }
 
 void		Listen::CheckPortNumber(std::string port_number) {			
-	(void)port_number;
-	// std::cout << "in check port number with: >" << port_number << "<" << std::endl;
 	if (port_number.find_first_not_of("0123456789") != std::string::npos)
 		throw InvalidPortException();
 	_port_number = port_number;
+	// do extra check to see if > 0 and < 66....
 }
 
 void		Listen::CheckIpAddress(std::string ip_address) {
@@ -72,6 +81,7 @@ void		Listen::CheckIpAddress(std::string ip_address) {
 			throw InvalidIpException();
 		_ip_address = ip_address;
 	}
+	// do an extra check by converting the IP with inet_pton function and if result is -1 it is NOT valid.
 }
 
 std::string		Listen::getPortNumber() {
