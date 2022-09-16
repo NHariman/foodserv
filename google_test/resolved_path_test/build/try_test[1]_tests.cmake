8a9636469209de18728d89f2c773ffdf75160008
Sanne Albreghs <55091762+salbregh@users.noreply.github.com>
2022-09-16 20:30:50 +0200

    test check

---
+++ google_test/resolved_path_test/build/try_test[1]_tests.cmake
@@ -0,0 +1,3 @@
+add_test( RootTest.RootTest /Users/sannealbreghs/Desktop/foodserv/google_test/resolved_path_test/build/try_test [==[--gtest_filter=RootTest.RootTest]==] --gtest_also_run_disabled_tests)
+set_tests_properties( RootTest.RootTest PROPERTIES WORKING_DIRECTORY /Users/sannealbreghs/Desktop/foodserv/google_test/resolved_path_test/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set( try_test_TESTS RootTest.RootTest)
