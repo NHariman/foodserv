94ad0f7394a50df272668169078ecc8ce1979809
Niks Hariman <nhariman@hotmail.com>
2022-09-27 21:24:21 +0200

    made beans spin

--- src/err/error_responses.hpp
+++ src/err/error_responses.hpp
@@ -2,11 +2,12 @@
 # define ERROR_RESPONSES_HPP
 
 #define CRLF "\r\n"
-#define footer "<hr><center>foodserv 1.0<br><img src=\"img/bean_flip_gif.gif\"></center>\r\n</html>"
 #define STYLE_START "<style>"
+#define IMG_CSS "img {\r\nwidth: 35%;\r\n height: auto;\r\n}\r\nbody{color:white;background-color:black;font-family:\'Trebuchet MS\';}"
+#define KEYFRAME_SPIN "@keyframes spin {from {transform:rotate(0deg); }to {transform:rotate(360deg);}}"
+#define SPIN_DIV ".spin {width: 5%;\r\nanimation-name: spin;animation-duration: 4000ms;animation-iteration-count: infinite;animation-timing-function: linear;}"
 #define STYLE_END "</style>"
-#define IMG_CSS "img {\r\nwidth: 80%;\r\n height: auto;\r\n}"
-
+#define footer "<hr><center>foodserv 1.0<br><img src=\"img/beans_flip_gif.gif\" class=\"spin\"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</center>\r\n</html>"
 
 char*	GetServerErrorPage(int status_code);
 
--- src/err/main.cpp
+++ src/err/main.cpp
@@ -1,7 +1,12 @@
 #include "error_responses.hpp"
 #include <iostream>
 
-int	main() {
-	std::cout << GetServerErrorPage(507) << std::endl;
+int	main(int ac, char **av) {
+	if (ac < 2 || ac > 2)
+		std::cout << GetServerErrorPage(508) << std::endl;
+	else {
+		int a = atoi(av[1]);
+		std::cout << GetServerErrorPage(a) << std::endl;
+	}
 	return 0;
 }
