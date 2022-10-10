15b7d7d25e64ab0f56a813fc461afc180ffeebcb
Michelle Jiam <55353487+MichelleJiam@users.noreply.github.com>
2022-10-10 17:10:46 +0200

    Changed CanCloseConnection to CanClose

--- src/connection/connection.hpp
+++ src/connection/connection.hpp
@@ -15,7 +15,7 @@ class Connection {
 
 		void	Receive(char const* buffer);
 		void	Dispatch();
-		bool	CanCloseConnection() const;
+		bool	CanClose() const;
 		// debug
 		Response const& DebugGetResponse();
 
