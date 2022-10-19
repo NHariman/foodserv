78b462102133e893b4f8fc464cc1fcffd7586bfe
Sanne Albreghs <55091762+salbregh@users.noreply.github.com>
2022-10-19 15:36:53 +0200

    resolved path test

--- google_test/resolved_path_test/try_test.cpp
+++ google_test/resolved_path_test/try_test.cpp
@@ -1,106 +1,167 @@
 #include <gtest/gtest.h>
 #include "../../src/config/nginx_config.hpp"
 #include "../../src/resolved_target/target_config.hpp"
+#include "../../src/request/request.hpp"
 
+// RETURN TESTING
 TEST(ReturnTest, ResolvedPathTesting) {
 
-	NginxConfig		test("/Users/sannealbreghs/Desktop/foodserv/config_files/test_config/return.conf");
-	TargetConfig	target;
+	NginxConfig		test("../config_files/return.conf");
+	Request	request(&test);
 
 {
+	TargetConfig target = request.GetTargetConfig();
 	target.Setup(&test, "localhost", "80", "/");
-	EXPECT_EQ(target.GetResolvedPath(), "/test/deze/shit");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "/test/deze/shit");
 }
+
 {
+	TargetConfig target = request.GetTargetConfig();
 	target.Setup(&test, "localhost", "100", "/");
-	EXPECT_EQ(target.GetResolvedPath(), "/helemaal/onder");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "/helemaal/onder");
+}
+
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "100", "/niks");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/niks", "GET"), "/helemaal/onder");
+}
+
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "8080", "/hoi");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/hoi", "GET"), "/test/return/in/location");
 }
 {
-	target.Setup(&test, "localhost", "100", "/");
-	EXPECT_EQ(target.GetResolvedPath(), "/helemaal/onder");
-}
-{
-	target.Setup(&test, "localhost", "8080", "/test");
-	EXPECT_EQ(target.GetResolvedPath(), "/test/return/in/location");
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "8080", "/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "/test/return/in/location");
 }
 }
 
-
+// ROOT TESTING
 TEST(RootTest, ResolvedPathTesting) {
-	NginxConfig		test("/Users/sannealbreghs/Desktop/foodserv/config_files/test_config/root.conf");
-	TargetConfig	target;
+	NginxConfig		test("../config_files/root.conf");
+	Request	request(&test);
+
 {
-	target.Setup(&test, "localhost", "80", "/hoi");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/hoi");
-}
-{
-	target.Setup(&test, "localhost", "80", "/hoi/otherroot");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/hoi/otherroot");
-}
-{
-	target.Setup(&test, "localhost", "80", "/doei/he");
-	EXPECT_EQ(target.GetResolvedPath(), "/Crap/doei/he");
-}
-{
-	target.Setup(&test, "localhost", "80", "/moreslashes");
-	EXPECT_EQ(target.GetResolvedPath(), "/Slash/queen/moreslashes");
-}
-{
-	target.Setup(&test, "localhost", "80", "/dir/more");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/dir/more");
-}
-{
+	TargetConfig target = request.GetTargetConfig();
 	target.Setup(&test, "localhost", "80", "/");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/otherpage.html");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "../www/html/otherpage.html");
 }
 {
-	target.Setup(&test, "localhost", "8888", "/Desktop/foodserv/www/html/");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/index.html");
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "80", "/coffee_and_beans/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/coffee_and_beans/", "GET"), "../www/html/coffee_and_beans/otherpage.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "8080", "/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "../www/html/index.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "8080", "/coffee_and_beans/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/coffee_and_beans/", "GET"), "../www/html/coffee_and_beans/index.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "test_second", "8080", "/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "../www/html/otherpage.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "test_second", "8080", "/coffee_and_beans/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/coffee_and_beans/", "GET")
+, "../www/html/coffee_and_beans/otherpage.html");
+}
+
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "666", "/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "../www/html/index.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "666", "/img/beans.png");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/img/beans.png", "GET"), "../www/img/beans.png");
 }
 }
 
-TEST(AliasTest, ResolvedPathTesting) {
-	NginxConfig		test("/Users/sannealbreghs/Desktop/foodserv/config_files/test_config/alias.conf");
-	TargetConfig	target;
-
-{
-	target.Setup(&test, "localhost", "80", "/html-pages/index.html");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/index.html");
-}
-{
-	target.Setup(&test, "localhost", "80", "/html-pages");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/index.html");
-}
-{
-	target.Setup(&test, "test", "888", "/pages/");
-	EXPECT_EQ(target.GetResolvedPath(), "");
-}
-{
-	target.Setup(&test, "test", "888", "/test2/index.html");
-	EXPECT_EQ(target.GetResolvedPath(), "/User/sannealbreghs/Desktop/foodserv/www/html/index.html");
-}
-{
-	target.Setup(&test, "noname", "888", "/config_files/test_config/alias.conf");
-	EXPECT_EQ(target.GetResolvedPath(), "/User/sannealbreghs/Desktop/foodserv/config_files/test_config/alias.conf");
-}
-}
 
 TEST (AutoIndex, ResolvedPathTesting) {
-	NginxConfig		test("/Users/sannealbreghs/Desktop/foodserv/config_files/test_config/root.conf");
-	TargetConfig	target;
+	NginxConfig		test("../config_files/root.conf");
+	Request	request(&test);
 {
-	target.Setup(&test, "local", "666", "/");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/");
-	EXPECT_EQ(target.MustGenerateIndex(), true);
-}
-{
-	target.Setup(&test, "localhost", "8080", "/auto/on/");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/auto/on/");
-	EXPECT_EQ(target.MustGenerateIndex(), true);
-}
-{
-	target.Setup(&test, "localhost", "666", "/HTML/");
-	EXPECT_EQ(target.GetResolvedPath(), "/Users/sannealbreghs/Desktop/foodserv/www/html/otherpage.html");
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "777", "/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "");
 	EXPECT_EQ(target.MustGenerateIndex(), false);
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "777", "/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", "GET"), "");
+	EXPECT_EQ(target.MustGenerateIndex(), false);
+}
+
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "777", "/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/", ""), "../www/html/");
+	EXPECT_EQ(target.MustGenerateIndex(), false);
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "test_2", "777", "/coffee_and_beans/");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/coffee_and_beans/", "GET"), "../www/html/coffee_and_beans/");
+	EXPECT_EQ(target.MustGenerateIndex(), true);	
+}
+}
+
+// ALIAS TEST
+TEST (Alias, ResolvedPathTesting) {
+	NginxConfig		test("../config_files/alias.conf");
+	Request	request(&test);
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "80", "/html-pages/index.html");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/html-pages/index.html", "GET"), "../www/html/index.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "80", "/html-pages");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/html-pages", "GET"), "../www/html/index.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "localhost", "80", "/html-pages");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/html-pages", "GET"), "../www/html/index.html");
+}
+{
+	TargetConfig target = request.GetTargetConfig();
+	target.Setup(&test, "test_2", "80", "/www/crap/whoo/coffee_and_beans/otherpage.html");
+	ResolvedPath	resolved_path(&target);
+	EXPECT_EQ(resolved_path.Resolve("/www/crap/whoo/coffee_and_beans/otherpage.html", "GET"), "/www/html/www/crap/whoo/coffee_and_beans/otherpage.html");
 }
 }
